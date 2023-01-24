1feb96470e81f6881a5cd500481636f9b707eab2
crosscay <caicedocarlos0306@gmail.com>
2023-01-24 13:06:14 -0300

    actualizando url server

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://server-productos-node-express.herokuapp.com/api/productos/'
+  baseUrl: 'https://server-node-productos-mean.up.railway.app/api/productos/'
 };
